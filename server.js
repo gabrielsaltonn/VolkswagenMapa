5fa79f914c2a5ac515fb719f4677510f2840ec27
gabrielsaltonn <gbl.salton@gmail.com>
2026-06-20 20:12:21 -0300

    Ajusta limite de payload para fotos

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.static("public"));
 
 app.use("/api/printers", printerRoutes);
